90d265e70116e9463241c5a0b14e2f4e9c05e809
fritzmg <fmg@inspiredminds.at>
2025-07-15 12:33:56 +0100

    use locking

--- src/NewsArchiver.php
+++ src/NewsArchiver.php
@@ -12,34 +12,34 @@ declare(strict_types=1);
 
 namespace InspiredMinds\ContaoNewsArchiving;
 
+use Contao\CoreBundle\DependencyInjection\Attribute\AsCallback;
+use Contao\CoreBundle\DependencyInjection\Attribute\AsCronJob;
+use Contao\CoreBundle\DependencyInjection\Attribute\AsHook;
 use Contao\CoreBundle\Framework\ContaoFramework;
-use Contao\CoreBundle\ServiceAnnotation\Callback;
-use Contao\CoreBundle\ServiceAnnotation\CronJob;
-use Contao\CoreBundle\ServiceAnnotation\Hook;
 use Contao\NewsArchiveModel;
 use Contao\StringUtil;
 use Doctrine\DBAL\Connection;
 use Psr\Log\LoggerInterface;
+use Symfony\Component\Lock\LockFactory;
 
 /**
  * Executes news archiving according to each news archive's settings.
- *
- * @Callback(table="tl_news", target="config.onload")
- * @Callback(table="tl_news_archive", target="config.onload")
- *
- * @CronJob("minutely")
- *
- * @Hook("getPageLayout")
  */
+#[AsCronJob('minutely')]
+#[AsHook('getPageLayout')]
+#[AsCallback('tl_news', 'config.onload')]
+#[AsCallback('tl_news_archive', 'config.onload')]
 class NewsArchiver
 {
     // Whether archiving was done already in this process.
     protected static bool $archived = false;
 
     public function __construct(
-        private readonly LoggerInterface $generalLogger,
+        private readonly LoggerInterface $contaoCronLogger,
         private readonly Connection $db,
         private readonly ContaoFramework $contaoFramework,
+        private readonly LockFactory $lockFactory,
+        private readonly string $projectDir,
     ) {
     }
 
@@ -50,54 +50,65 @@ class NewsArchiver
             return;
         }
 
-        $this->contaoFramework->initialize();
+        // Check if there is another archiving process already running
+        $lock = $this->lockFactory->createLock(md5($this->projectDir.'-news-archiver'));
 
-        // Get all news archives where archiving is active
-        $archives = NewsArchiveModel::findBy(
-            [
-                "archiving = '1'",
-                'archivingTarget != 0',
-                "(archivingTime != '' OR archivingStop = 1)",
-            ],
-            [],
-            ['order' => 'title ASC'],
-        );
-
-        // Go through each archive
-        foreach ($archives ?? [] as $archive) {
-            // Get the target archive
-            $target = NewsArchiveModel::findById($archive->archivingTarget);
-
-            if (null === $target) {
-                continue;
-            }
-
-            $archivingTime = StringUtil::deserialize($archive->archivingTime, true);
-            $value = $archivingTime['value'] ?? null;
-            $unit = $archivingTime['unit'] ?? null;
-
-            // Move according to time
-            if ($value && $unit && ($time = strtotime('-'.$value.' '.$unit))) {
-                $result = $this->db->executeQuery('UPDATE tl_news SET pid = ? WHERE pid = ? AND time < ?', [$target->id, $archive->id, $time]);
-                $count = $result->rowCount();
-
-                if ($count > 0) {
-                    $this->generalLogger->info('Moved '.$count.' news entries from "'.$archive->title.'" to "'.$target->title.'" due to time criteria.');
-                }
-            }
-
-            // Move according to stop time
-            if ($archive->archivingStop) {
-                $result = $this->db->executeQuery("UPDATE tl_news SET pid = ?, stop = '' WHERE pid = ? AND stop != '' AND stop < UNIX_TIMESTAMP()", [$target->id, $archive->id]);
-                $count = $result->rowCount();
-
-                if ($count > 0) {
-                    $this->generalLogger->info('Moved '.$count.' news entries from "'.$archive->title.'" to "'.$target->title.'" due to stop criteria.');
-                }
-            }
+        if (!$lock->acquire()) {
+            return;
         }
 
-        // Save that archiving was executed
-        self::$archived = true;
+        try {
+            $this->contaoFramework->initialize();
+
+            // Get all news archives where archiving is active
+            $archives = NewsArchiveModel::findBy(
+                [
+                    "archiving = '1'",
+                    'archivingTarget != 0',
+                    "(archivingTime != '' OR archivingStop = 1)",
+                ],
+                [],
+                ['order' => 'title ASC'],
+            );
+
+            // Go through each archive
+            foreach ($archives ?? [] as $archive) {
+                // Get the target archive
+                $target = NewsArchiveModel::findById($archive->archivingTarget);
+
+                if (null === $target) {
+                    continue;
+                }
+
+                $archivingTime = StringUtil::deserialize($archive->archivingTime, true);
+                $value = $archivingTime['value'] ?? null;
+                $unit = $archivingTime['unit'] ?? null;
+
+                // Move according to time
+                if ($value && $unit && ($time = strtotime('-'.$value.' '.$unit))) {
+                    $result = $this->db->executeQuery('UPDATE tl_news SET pid = ? WHERE pid = ? AND time < ?', [$target->id, $archive->id, $time]);
+                    $count = $result->rowCount();
+
+                    if ($count > 0) {
+                        $this->contaoCronLogger->info('Moved '.$count.' news entries from "'.$archive->title.'" to "'.$target->title.'" due to time criteria.');
+                    }
+                }
+
+                // Move according to stop time
+                if ($archive->archivingStop) {
+                    $result = $this->db->executeQuery("UPDATE tl_news SET pid = ?, stop = '' WHERE pid = ? AND stop != '' AND stop < UNIX_TIMESTAMP()", [$target->id, $archive->id]);
+                    $count = $result->rowCount();
+
+                    if ($count > 0) {
+                        $this->contaoCronLogger->info('Moved '.$count.' news entries from "'.$archive->title.'" to "'.$target->title.'" due to stop criteria.');
+                    }
+                }
+            }
+
+            // Save that archiving was executed
+            self::$archived = true;
+        } finally {
+            $lock->release();
+        }
     }
 }
